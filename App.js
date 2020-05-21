dc5737874cc65e178627e9c06c2636d0e9ca95a2
Kevin <kevin.yi93@gmail.com>
2020-05-21 16:57:30 -0400

    Link global Redux store and Redux Devtools to <App>

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import reducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import middleware from './middleware';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>This is the app</Text>
-      </View>
+      <Provider store={createStore(reducer, composeWithDevTools(middleware))}>
+        <View>
+          <Text>This is the app</Text>
+        </View>
+      </Provider>
     );
   }
 }
