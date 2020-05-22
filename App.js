3ef0e894332339fc3849a466537f0d1fdf186416
Kevin <kevin.yi93@gmail.com>
2020-05-21 20:06:52 -0400

    Render the main menu screen

--- App.js
+++ App.js
@@ -5,13 +5,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import middleware from './middleware';
+import MainMenu from './components/MainMenu'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer, composeWithDevTools(middleware))}>
         <View>
-          <Text>This is the app</Text>
+          <MainMenu />
         </View>
       </Provider>
     );
---
+++ components/MainMenu.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+class MainMenu extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          This is the Main Menu
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default MainMenu
