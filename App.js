42da242b66aab7fa426611f65ecdcb8dc3ca9edf
Kevin <kevin.yi93@gmail.com>
2020-05-21 20:23:55 -0400

    Create a dark gray status bar for the entire scope of the app

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import reducer from './reducers';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import middleware from './middleware';
 import MainMenu from './components/MainMenu'
+import Constants from "expo-constants";
+import { darkGray } from './utils/colors'
+
+
+function PokerStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer, composeWithDevTools(middleware))}>
         <View style={{ flex: 1 }}>
+          <PokerStatusBar backgroundColor={darkGray} barStyle="light-content" />
           <MainMenu />
         </View>
       </Provider>
