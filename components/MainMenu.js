d69ddf1cba7a7d9a928706beec439cee1590baa5
Kevin <kevin.yi93@gmail.com>
2020-05-21 22:17:50 -0400

    Stylize main menu items with vector icons and color

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
 import { Header } from 'react-native-elements'
-import { darkGray } from '../utils/colors'
+import { darkGray, backgroundGray, menuItemGray, limeGreen } from '../utils/colors'
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 class MainMenu extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Header
-          centerComponent={{ text: 'Main Menu', style: { color: '#fff' } }}
+          leftComponent={<MaterialCommunityIcons
+            name="poker-chip"
+            size={30}
+            color='white'
+          />}
+          centerComponent={{ text: 'Main Menu', style: { color: '#fff', fontSize: 24 } }}
           containerStyle={{
             backgroundColor: darkGray,
             justifyContent: 'space-around',
           }} />
-        <Text>
-          This is the Main Menu
-        </Text>
+        <View style={styles.menuContent}>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuText}>Add a Session</Text>
+            <Entypo
+              style={{ alignSelf: 'center' }}
+              name="circle-with-plus"
+              size={24}
+              color='black' />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuText}>Total Results</Text>
+            <Entypo
+              style={{ alignSelf: 'center' }}
+              name="credit"
+              size={24}
+              color={limeGreen} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuText}>All sessions</Text>
+            <Ionicons
+              style={{ alignSelf: 'center' }}
+              name="md-list-box"
+              size={26}
+              color="black" />
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
@@ -25,7 +56,28 @@ export default MainMenu
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center'
+    backgroundColor: backgroundGray
+  },
+  menuContent: {
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  menuItem: {
+    width: Dimensions.get('window').width,
+    borderColor: 'black',
+    backgroundColor: menuItemGray,
+    borderRadius: 5,
+    borderWidth: 2,
+    height: 80,
+    margin: 20,
+  },
+  menuText: {
+    marginTop: 10,
+    marginBottom: 5,
+    fontSize: 20,
+    color: '#d0d0d0',
+    justifyContent: 'center',
+    alignSelf: 'center'
   }
 })
