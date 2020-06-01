51ffda3f42ae31ceaf68bacbd5c2762974cfe80d
Kevin <kevin.yi93@gmail.com>
2020-06-01 14:25:23 -0400

    Add import button that will alert user whether JSON file was successfully imported or not

--- components/ImportData.js
+++ components/ImportData.js
@@ -1,21 +1,96 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, TextInput, Dimensions, ScrollView } from 'react-native';
-import { darkGray, backgroundGray, menuItemGray, limeGreen } from '../utils/colors';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, ScrollView, Alert } from 'react-native';
+import { darkGray, backgroundGray } from '../utils/colors';
 import { Header } from 'react-native-elements';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { receiveSessions } from '../actions'
+import { connect } from 'react-redux'
+
+function ImportBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      style={Platform.OS === 'ios'
+        ? styles.iosSubmitBtn
+        : styles.androidSubmitBtn}
+      onPress={onPress}>
+      <Text style={styles.submitBtnText}>Submit</Text>
+    </TouchableOpacity>
+  )
+}
 
 class ImportData extends Component {
   constructor(props) {
     super(props);
     this.state = {
       text: '',
-      testWidth: '99%'
+      testWidth: '99%',
     }
   }
   componentDidMount() {
     setTimeout(() => {
       this.setState({ testWidth: '100%' })
     }, 100)
+  }
+
+  importSuccessAlert = () =>
+    Alert.alert(
+      "Success!",
+      "Your Data has been imported into the app!",
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );
+
+  importFailAlert = () =>
+    Alert.alert(
+      "Error!",
+      "Data is corrupted.",
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );
+
+  submitData = () => {
+    const { text } = this.state
+    const importData = JSON.parse(text)
+    const { dispatch } = this.props
+    const keys = Object.keys(importData)
+    let isDateValid = false
+    let isResultValid = false
+    let key = keys[0]
+    for (let i = 1; i < keys.length; i++) {
+      if (keys[i] - key !== 1) {
+        this.setState(() => ({
+          text: ''
+        }))
+        this.importFailAlert()
+        return
+      }
+      key = keys[i]
+    }
+    for (let session in importData) {
+      const dateString = importData[session].date
+      const dateObj = new Date(dateString)
+      const dateObjString = (JSON.stringify(dateObj.toJSON()).slice(0, 11).slice(1))
+      isDateValid = dateString === dateObjString
+      isResultValid = typeof importData[session].result === 'number'
+      if (!isDateValid || !isResultValid) {
+        isDatevalid = false
+        isResultValid = false
+        break
+      }
+    }
+    if (isDateValid && isResultValid) {
+      dispatch(receiveSessions(importData))
+      this.importSuccessAlert()
+    } else {
+      this.importFailAlert()
+    }
+    this.setState(() => ({
+      text: ''
+    }))
   }
   render() {
     return (
@@ -48,11 +123,12 @@ class ImportData extends Component {
 
             }}
             placeholderTextColor='white'
-            placeholder="Type here to translate!"
+            placeholder="Paste Code Here"
             onChangeText={(text) => this.setState({ text })}
             value={this.state.text}
           />
         </ScrollView>
+        <ImportBtn onPress={this.submitData} />
       </View>
     )
   }
@@ -63,6 +139,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: backgroundGray
   },
+  iosSubmitBtn: {
+    backgroundColor: darkGray,
+    borderColor: 'gray',
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 7,
+    height: 60,
+    marginLeft: 40,
+    marginRight: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 50
+  },
+  androidSubmitBtn: {
+    backgroundColor: 'blue',
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 60,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  submitBtnText: {
+    color: 'white',
+    fontSize: 22,
+    textAlign: 'center',
+  },
 })
 
-export default ImportData
+export default connect()(ImportData)
